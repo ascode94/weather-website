49c2f6071d71df78a3b2f13e17c5a8dd59f892ad
Alireza Sharifpour <alireza@Alirezas-MacBook-Pro.local>
2019-07-17 18:58:37 +0430

    Add new forecast data to the application

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,10 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of the rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out, today's temperature low is ${body.daily.data[0].temperatureLow} and today's temperature high is ${body.daily.data[0].temperatureHigh} There is ${body.currently.precipProbability}% chance of the rain`)
         }
     })
 }
   
 
-module.exports = forecast
+module.exports = forecast 
